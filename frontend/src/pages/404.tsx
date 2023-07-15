697c5a06c5f99897534098cef9e78ac3a64a7c2e
Athicha L. (mac) <nongtonkaew@gmail.com>
2023-07-16 03:09:31 +0700

    Fixed 404 Responsive

--- frontend/src/pages/404.tsx
+++ frontend/src/pages/404.tsx
@@ -2,7 +2,7 @@ export default function Page() {
   return (
     <>
       <div className="relative mt-[35vh]">
-        <h1 className="w-full text-center text-3xl leading-relaxed">
+        <h1 className="w-3/4 mx-auto text-center text-3xl leading-relaxed">
           ขออภัย เรากำลังพัฒนาเว็บไซต์ให้
           <span className="text-green-700">ดีขึ้น</span> 🚧
         </h1>
