0aa9ac18694da3ae89fe364ac445f5d3ab9ea4d9
Athicha L. (mac) <nongtonkaew@gmail.com>
2023-07-16 01:45:24 +0700

    Added 404 Page

---
+++ frontend/src/pages/404.tsx
@@ -0,0 +1,12 @@
+export default function Page() {
+  return (
+    <>
+      <div className="relative mt-[35vh]">
+        <h1 className="w-full text-center text-3xl leading-relaxed">
+          ขออภัย เรากำลังพัฒนาเว็บไซต์ให้
+          <span className="text-green-700">ดีขึ้น</span> 🚧
+        </h1>
+      </div>
+    </>
+  )
+}
