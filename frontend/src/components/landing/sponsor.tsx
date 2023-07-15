89016a36e0f75c8a9c9b30055bd26a7bdb9410d4
Athicha L. (mac) <nongtonkaew@gmail.com>
2023-07-16 02:32:54 +0700

    Updated Sponsor Section

--- frontend/src/components/landing/sponsor.tsx
+++ frontend/src/components/landing/sponsor.tsx
@@ -8,21 +8,20 @@ import TWA from "@/../public/TWA_sponsor.svg"
 export default function Sponsor() {
   const sponsors = {
     top: [
-      { name: "TWA", logo: TWA },
-      { name: "TWA", logo: TWA },
-      { name: "TWA", logo: TWA },
-      { name: "TWA", logo: TWA },
-      { name: "TWA", logo: TWA },
-      { name: "TWA", logo: TWA },
-      { name: "TWA", logo: TWA },
-      { name: "TWA", logo: TWA },
+      { name: "TWA", logo: TWA, class: "bg-black text-white" },
+      { name: "TWA", logo: TWA, class: "bg-black text-white" },
+      { name: "TWA", logo: TWA, class: "bg-black text-white" },
+      { name: "TWA", logo: TWA, class: "bg-black text-white" },
+      { name: "TWA", logo: TWA, class: "bg-black text-white" },
+      { name: "TWA", logo: TWA, class: "bg-black text-white" },
+      { name: "TWA", logo: TWA, class: "bg-black text-white" },
     ],
     medium: [],
     bottom: [],
   }
 
   return (
-    <section className="font-Athiti font-semibold text-ds-secondary text-4xl text-center border-red-500 border">
+    <section className="font-Athiti w-full font-semibold text-ds-secondary text-4xl text-center border-red-500 border">
       <h2>
         ได้รับการรองรับ
         <br />
@@ -30,27 +29,33 @@ export default function Sponsor() {
       </h2>
 
       <div className="font-Athiti font-medium text-black text-2xl p-2 relative py-8">
-        {/* <div className="mb-4 flex gap-2 overflow-clip -translate-x-[20px]">
+        {/* <div className="mb-4 flex gap-4 overflow-clip -translate-x-[20px]">
           {sponsors.top.map((s) => (
             <div
               key={s.name}
-              className="bg-white z-50 py-1 rounded px-4 flex items-center gap42"
+              className={`z-50 py-1 relative rounded px-4 flex items-center gap-2 ${s.class}`}
             >
-              <Image src={Icon} className="h-4 w-4" alt="TWA Logo" />
-              <span className="w-fit">{s.name}</span>
+              <Image
+                src={s.logo}
+                className="h-full w-auto"
+                fill
+                alt="TWA Logo"
+              />
+              <span className="text-transparent w-fit pr-4">{s.name}</span>
             </div>
           ))}
         </div> */}
-        <Image src={placeholder} alt="Sponsor placeholder"></Image>
 
         {/* Shadow */}
-        <div
+        {/* <div
           className="left-1/2 -z-10 -translate-x-1/2 top-1/2 -translate-y-1/2 absolute h-[90%] w-full"
           style={{
             background:
               "linear-gradient(180deg, rgba(255,255,255,1) 0%,rgba(0,0,0,0.05) 25%, rgba(0,0,0,0.05) 50%,rgba(0,0,0,0.05) 75%, rgba(255,255,255,1) 100%)",
           }}
-        ></div>
+        ></div> */}
+
+        <Image src={placeholder} alt="Sponsor Section"/>
       </div>
     </section>
   )
