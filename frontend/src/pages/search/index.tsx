1dc5c6e594d310a217e1b7386d6c1f5965f8a751
Athicha L. (mac) <nongtonkaew@gmail.com>
2023-07-15 16:03:51 +0700

    Added Districts option

--- frontend/src/pages/search/index.tsx
+++ frontend/src/pages/search/index.tsx
@@ -1,15 +1,39 @@
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
 import { useEffect, useState } from "react"
 
 export default function Page() {
-  const [provinces, setProvinces] = useState([])
+  interface province {
+    PROVINCE_ID: number
+    PROVINCE_CODE: string
+    PROVINCE_NAME: string
+    GEO_ID: number
+  }
+
+  interface district {
+    DISTRICT_ID: number
+    DISTRICT_CODE: string
+    DISTRICT_NAME: string
+    GEO_ID: number
+    PROVINCE_ID: number
+  }
+
+  const [provinces, setProvinces] = useState<province[]>([])
+  const [districts, setDistricts] = useState<district[]>([])
+  const [filterDistricts, setFilterDistricts] = useState<district[]>([])
+  const [provinceId, setProvinceId] = useState("")
+  const [districtId, setDistrictId] = useState("")
 
   const getProvinces = async () => {
     const res = await fetch("/provinces.json")
     const json = await res.json()
 
-    console.log(json)
+    const res2 = await fetch("/districts.json")
+    const json2 = await res2.json()
 
     setProvinces(json)
+    setDistricts(json2)
+    setFilterDistricts(json2.filter((d) => d.PROVINCE_ID == 3))
   }
 
   useEffect(() => {
@@ -20,33 +44,65 @@ export default function Page() {
     }
   }, [])
 
+  function onSelectProvince(provinceId: number) {
+    setFilterDistricts(
+      [...districts].filter((d) => d.PROVINCE_ID == provinceId)
+    )
+  }
+
   return (
     <>
-      <div className="min-h-screen flex justify-center items-center flex-col">
-        <h1 className="text-5xl py-8">ค้นหาโฉนด</h1>
-        <div className="w-4/5">
+      <div className="min-h-screen flex justify-center items-center flex-col -m-4">
+        <h1 className="text-5xl py-8">ค้นหาพื้นที่</h1>
+        <div className="w-4/5 max-w-sm">
           <p>เลือกจังหวัด</p>
-          <select>
+          <select
+            className="px-2 py-4 border w-full mb-2"
+            onChange={(e) => onSelectProvince(Number(e.target.value))}
+          >
+            <option disabled selected>
+              จังหวัด
+            </option>
             {provinces.map((p) => (
-              <option key={p.PROVINCE_ID} value={p.PROVINCE_CODE}>
+              <option
+                key={p.PROVINCE_ID}
+                value={p.PROVINCE_ID}
+                selected={p.PROVINCE_NAME == "นนทบุรี"}
+              >
                 {p.PROVINCE_NAME}
               </option>
             ))}
           </select>
 
           <p>อำเภอ</p>
-          <select>
-            {provinces.map((p) => (
-              <option key={p.PROVINCE_ID} value={p.PROVINCE_CODE}>
-                {p.PROVINCE_NAME}
+          <select
+            className="px-2 py-4 border w-full mb-2"
+            onChange={(e) => setDistrictId(e.target.value)}
+          >
+            <option disabled selected>
+              อำเภอ
+            </option>
+            {filterDistricts.map((p) => (
+              <option key={p.DISTRICT_ID} value={p.DISTRICT_CODE}>
+                {p.DISTRICT_NAME}
               </option>
             ))}
           </select>
+
+          <input
+            className="px-2 py-4 border w-full mb-2"
+            placeholder="เลขที่โฉนด"
+          ></input>
         </div>
 
-        <button className="w-4/5 py-4 bg-violet-500 text-white rounded-2xl text-3xl">
-          ค้นหา
-        </button>
+        <Link
+          href={`/search/map?province=${provinceId}&district=${districtId}`}
+          className="w-4/5"
+        >
+          <Button size={"lg"} className="w-full text-2xl py-8">
+            ค้นหา
+          </Button>
+        </Link>
       </div>
     </>
   )
