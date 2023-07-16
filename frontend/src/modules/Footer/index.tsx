e02cb4ee3fb93dd8df24028701f78c4cc51a3796
Athicha L. (mac) <nongtonkaew@gmail.com>
2023-07-16 10:00:39 +0700

    Center Footer

--- frontend/src/modules/Footer/index.tsx
+++ frontend/src/modules/Footer/index.tsx
@@ -17,66 +17,68 @@ import Gmail from "@/../public/images/Gmail.webp"
 export default function Footer() {
   return (
     <footer className="bg-[#295729] text-white p-4">
-      <div>
-        <Accordion type="single" collapsible className="w-full">
-          <AccordionItem value="item-1">
-            <Link href="/about">
+      <div className="mx-auto max-w-4xl">
+        <div>
+          <Accordion type="single" collapsible className="w-full">
+            <AccordionItem value="item-1">
+              <Link href="/about">
+                <AccordionTrigger className="text-ds-primary">
+                  เกี่ยวกับการันTREE
+                </AccordionTrigger>
+              </Link>
+              <AccordionContent></AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-2">
               <AccordionTrigger className="text-ds-primary">
-                เกี่ยวกับการันTREE
+                บริการลูกค้า
               </AccordionTrigger>
-            </Link>
-            <AccordionContent></AccordionContent>
-          </AccordionItem>
-          <AccordionItem value="item-2">
-            <AccordionTrigger className="text-ds-primary">
-              บริการลูกค้า
-            </AccordionTrigger>
-            <AccordionContent>
-              <ul>
-                <li className="py-1">
-                  <Link href="faq">คำถามที่พบบ่อย</Link>
-                </li>
-                <li className="py-1">
-                  <Link href="insurance">บริการด้านกรมธรรม์</Link>
-                </li>
-                <li className="py-1">
-                  <Link href="claim">การเรียกร้องสินไหม</Link>
-                </li>
-                <li className="py-1">
-                  <Link href="payment">ช่องทางการชำระเบี้ยประกันภัย</Link>
-                </li>
-              </ul>
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
-      </div>
-      <div>
-        <h3 className="flex gap-2 font-bold items-center py-2">
-          การันTREE{" "}
-          <Image src={Logo} alt="Garantree Logo" className="h-8 w-8" />
-        </h3>
-        <p className="leading-tight">
-          เลขที่ 123 ถนนเพลินจิต เขตปทุมวัน กรุงเทพฯ 10330
-        </p>
-      </div>
-      <div className="text-ds-primary py-2">
-        © 2023 Untitled UI. All rights reserved.
-      </div>
-      <div className="flex gap-2">
-        <Link href="https://google.com">
-          <Image
-            src={Facebook}
-            alt="Garantree Facebook"
-            className="h-10 w-10"
-          />
-        </Link>
-        <Link href="https://google.com">
-          <Image src={Line} alt="Garantree Line" className="h-10 w-10" />
-        </Link>
+              <AccordionContent>
+                <ul>
+                  <li className="py-1">
+                    <Link href="faq">คำถามที่พบบ่อย</Link>
+                  </li>
+                  <li className="py-1">
+                    <Link href="insurance">บริการด้านกรมธรรม์</Link>
+                  </li>
+                  <li className="py-1">
+                    <Link href="claim">การเรียกร้องสินไหม</Link>
+                  </li>
+                  <li className="py-1">
+                    <Link href="payment">ช่องทางการชำระเบี้ยประกันภัย</Link>
+                  </li>
+                </ul>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        </div>
+        <div>
+          <h3 className="flex gap-2 font-bold items-center py-2">
+            การันTREE{" "}
+            <Image src={Logo} alt="Garantree Logo" className="h-8 w-8" />
+          </h3>
+          <p className="leading-tight">
+            เลขที่ 123 ถนนเพลินจิต เขตปทุมวัน กรุงเทพฯ 10330
+          </p>
+        </div>
+        <div className="text-ds-primary py-2">
+          © 2023 Untitled UI. All rights reserved.
+        </div>
+        <div className="flex gap-2">
+          <Link href="https://google.com">
+            <Image
+              src={Facebook}
+              alt="Garantree Facebook"
+              className="h-10 w-10"
+            />
+          </Link>
+          <Link href="https://google.com">
+            <Image src={Line} alt="Garantree Line" className="h-10 w-10" />
+          </Link>
 
-        <Link href="https://google.com">
-          <Image src={Gmail} alt="Garantree Gmail" className="h-10 w-10" />
-        </Link>
+          <Link href="https://google.com">
+            <Image src={Gmail} alt="Garantree Gmail" className="h-10 w-10" />
+          </Link>
+        </div>
       </div>
     </footer>
   )
