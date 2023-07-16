6b2ee9cd8152b75f2dfd3f2b0221417b35901aa8
Athicha L. (mac) <nongtonkaew@gmail.com>
2023-07-16 10:28:30 +0700

    Added Link to navbar

--- frontend/src/modules/Navbar/index.tsx
+++ frontend/src/modules/Navbar/index.tsx
@@ -6,6 +6,7 @@ import {
   SheetHeader,
   SheetTitle,
   SheetDescription,
+  SheetClose,
 } from "@/components/ui/sheet"
 import Burger from "../Burger"
 import { useEffect, useState } from "react"
@@ -37,16 +38,39 @@ const Navbar = () => {
                 </SheetTitle> */}
                 <SheetDescription>
                   <Accordion type="single" collapsible className="w-full">
-                    <Link href="/">
+                    <SheetClose asChild>
                       <span className="text-[14px] font-[500] text-left flex w-full h-[42px] py-[16px]">
-                        หน้าแรก
+                        <Link href="/" className="w-full h-full">
+                          หน้าแรก
+                        </Link>
                       </span>
-                    </Link>
-                    <Link href="/">
-                      <span className="text-[14px] font-[500] text-left flex w-full h-[42px] py-[16px]">
-                        หน้าแรก
-                      </span>
-                    </Link>
+                    </SheetClose>
+                    <AccordionItem value="item-2">
+                      <AccordionTrigger>กรมธรรม์</AccordionTrigger>
+                      <AccordionContent>
+                        <SheetClose asChild>
+                          <span className="text-[14px] font-[500] text-left flex w-full h-[42px] py-[16px]">
+                            <Link href="/register" className="w-full h-full">
+                              ลงทะเบียน
+                            </Link>
+                          </span>
+                        </SheetClose>
+                        <SheetClose asChild>
+                          <span className="text-[14px] font-[500] text-left flex w-full h-[42px] py-[16px]">
+                            <Link href="/check" className="w-full h-full">
+                              ตรวจสอบ
+                            </Link>
+                          </span>
+                        </SheetClose>
+                        <SheetClose asChild>
+                          <span className="text-[14px] font-[500] text-left flex w-full h-[42px] py-[16px]">
+                            <Link href="/claim" className="w-full h-full">
+                              เคลม
+                            </Link>
+                          </span>
+                        </SheetClose>
+                      </AccordionContent>
+                    </AccordionItem>
                   </Accordion>
                 </SheetDescription>
               </SheetHeader>
