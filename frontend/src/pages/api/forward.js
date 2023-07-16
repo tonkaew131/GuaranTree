5fe354e8b30a3632e1bcd383a56612622d4732b2
Phoomparin Mano <phoomparin@gmail.com>
2023-07-16 12:47:22 +0700

    STAFF COMMIT - fix builds

--- frontend/src/pages/api/forward.js
+++ frontend/src/pages/api/forward.js
@@ -1,12 +1,11 @@
 export default async function handler(req, res) {
-  // console.log(req.query)
+  const redirectReq = await fetch(
+    "http://bpt.dol.go.th:8088/geoserver/DOLNSL/wms?" +
+      new URLSearchParams(req.query)
+  )
 
-  const redirectReq = await fetch('http://bpt.dol.go.th:8088/geoserver/DOLNSL/wms' + new URLSearchParams(req.query))
-
-  // console.log('http://bpt.dol.go.th:8088/geoserver/DOLNSL/wms' + new URLSearchParams(req.query))
-  console.log(redirectReq)
-  // console.log(redirectReq.status)
-
-  // url: "http://bpt.dol.go.th:8088/geoserver/DOLNSL/wms",
-  return res.status(200).send(redirectReq.body)
+  return res
+    .status(200)
+    .setHeader("content-type", "image/png")
+    .send(Buffer.from(await redirectReq.arrayBuffer()))
 }
