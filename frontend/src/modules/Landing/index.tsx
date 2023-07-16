c12a1fc1275c6a1999c0ae5a7e0bf579dfd4477c
Athicha L. (mac) <nongtonkaew@gmail.com>
2023-07-16 09:06:24 +0700

    Added Landing metadata

--- frontend/src/modules/Landing/index.tsx
+++ frontend/src/modules/Landing/index.tsx
@@ -43,13 +43,16 @@ const Landing = () => {
     <>
       <Head>
         <title>การันTree</title>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
 
         <meta property="og:title" content="การันTree" />
         <meta
           property="og:description"
           content="การันTree ครอบคลุมทุกภัยพิบัติ ไม่มีค่าใช้จ่าย สมัครง่าย"
         />
-        <meta property="og:image" content="/images/TreeWideColor.webp" />
+        <meta property="og:image" content="/images/preview.webp" />
+        <meta name="theme-color" content="#67BE4D" />
+        <meta property="og:url" content="https://www.group-f.ywc.in.th/" />
       </Head>
 
       <div
