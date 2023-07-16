e6dceac2dbf212c161b5a0fc9588a8a797e9911f
Athicha L. (mac) <nongtonkaew@gmail.com>
2023-07-16 08:53:30 +0700

    Added href to icon in navbar

--- frontend/src/modules/Landing/index.tsx
+++ frontend/src/modules/Landing/index.tsx
@@ -5,6 +5,7 @@ import { Button } from "@/components/ui/button"
 import Helping from "@/components/landing/helping"
 import Sponsor from "@/components/landing/sponsor"
 import Step from "@/components/landing/step"
+import Head from "next/head"
 
 const Landing = () => {
   const containerRef = useRef<HTMLDivElement>(null)
@@ -39,101 +40,114 @@ const Landing = () => {
   }
 
   return (
-    <div
-      className="w-full h-full max-h-[calc(100vh-75px)] overflow-auto scrollbar-hide relative"
-      ref={containerRef}
-    >
-      <div
-        className="w-[60px] h-[60px] fixed right-[30px] bottom-[50px] z-[100] hover:opacity-70 cursor-pointer rounded-full shadow-lg"
-        onClick={() => {
-          window.location.href = "https://line.me/ti/p/@451zlmap"
-        }}
-      >
-        <Image src="/images/Line.png" alt="Picture of the land" fill />
-      </div>
-      <div
-        className="flex h-[100vh] fixed z-[-10]"
-        style={{
-          width: `${100 - scrollPosition / 3}vw`,
-          left: "0",
-          top: `${
-            scrollPosition > 300 ? 64 - (scrollPosition - 300) * 1 : 64
-          }px`,
-        }}
-      >
-        <Image
-          src="/images/TreeWild.webp"
-          alt="Picture of the land"
-          className="object-cover object-left"
-          fill
+    <>
+      <Head>
+        <title>การันTree</title>
+
+        <meta property="og:title" content="การันTree" />
+        <meta
+          property="og:description"
+          content="การันTree ครอบคลุมทุกภัยพิบัติ ไม่มีค่าใช้จ่าย สมัครง่าย"
         />
-      </div>
-      <div
-        className="flex h-[100vh] fixed top-[64px] z-[-20]"
-        style={{
-          width: "100vw",
-          left: "0vw",
-          top: `${
-            scrollPosition > 300 ? 64 - (scrollPosition - 300) * 1 : 64
-          }px`,
-        }}
-      >
-        <Image
-          src="/images/TreeWideColor.webp"
-          alt="Picture of the land"
-          className="object-cover object-left"
-          fill
-        />
-      </div>
+        <meta property="og:image" content="/images/TreeWideColor.webp" />
+      </Head>
 
       <div
-        className="flex h-[180px] fixed top-[64px] z-[-10]"
-        style={{
-          width: "100vw",
-          left: "0vw",
-          top: `${
-            scrollPosition > 300 ? 50 - (scrollPosition - 300) * 1 : 50
-          }px`,
-        }}
+        className="w-full h-full max-h-[calc(100vh-75px)] overflow-auto scrollbar-hide relative"
+        ref={containerRef}
       >
-        <div className="flex w-full h-[770px] flex-col justify-end">
-          <span className="text-[42px] font-[700] text-white font-Athiti px-[18px] leading-[50.40px]">
-            “<span className="text-[#67BE4D]">ตั้งต้นใหม่</span>
-            <br />
-            เมื่อไร่ประสบภัย
-            <br />
-            ด้วยประกันต้นไม้
-            <br />
-            ไว้ใจ
-            <span className="text-[#67BE4D]">การันTree</span>”
-          </span>
-          <span className="font-Athiti text-[20px] font-[500] text-white px-[18px] mt-[12px]">
-            ครอบคลุมทุกภัยพิบัติ ไม่มีค่าใช้จ่าย สมัครง่าย
-          </span>
+        <div
+          className="w-[60px] h-[60px] fixed right-[30px] bottom-[50px] z-[100] hover:opacity-70 cursor-pointer rounded-full shadow-lg"
+          onClick={() => {
+            window.location.href = "https://line.me/ti/p/@451zlmap"
+          }}
+        >
+          <Image src="/images/Line.png" alt="Picture of the land" fill />
         </div>
-      </div>
-      <Button
-        className="flex mt-[1120px] mb-[40px] h-[60px] w-[340px] max-w-[90%] ml-[18px] bg-[#67BE4D] hover:bg-[#457536] z-[5]"
-        onClick={toBottom}
-      >
-        ลองทำนายที่ดินดูเลย
-      </Button>
-      <div className="flex w-full h-[100vh] bg-black fixed top-[0px] z-[-30]" />
-      <Step />
-      <div className="relative flex w-full h-[100px] z-[-10] mb-[-2px]">
-        <Image src="/images/CurveBar.png" alt="Picture of the land" fill />
-      </div>
-      <div className="flex flex-col w-full h-fit bg-[#67BE4D] pb-[20px]">
-        <Helping scrollPosition={scrollPosition} />
-      </div>
-      <div className="w-full h-fit bg-white pt-[40px]">
-        <Sponsor />
-        <div className="flex flex-col w-full h-fit relative max-w-[500px] mx-auto">
-          <NewCard />
+        <div
+          className="flex h-[100vh] fixed z-[-10]"
+          style={{
+            width: `${100 - scrollPosition / 3}vw`,
+            left: "0",
+            top: `${
+              scrollPosition > 300 ? 64 - (scrollPosition - 300) * 1 : 64
+            }px`,
+          }}
+        >
+          <Image
+            src="/images/TreeWild.webp"
+            alt="Picture of the land"
+            className="object-cover object-left"
+            fill
+          />
         </div>
+        <div
+          className="flex h-[100vh] fixed top-[64px] z-[-20]"
+          style={{
+            width: "100vw",
+            left: "0vw",
+            top: `${
+              scrollPosition > 300 ? 64 - (scrollPosition - 300) * 1 : 64
+            }px`,
+          }}
+        >
+          <Image
+            src="/images/TreeWideColor.webp"
+            alt="Picture of the land"
+            className="object-cover object-left"
+            fill
+          />
+        </div>
+
+        <div
+          className="flex h-[180px] fixed top-[64px] z-[-10]"
+          style={{
+            width: "100vw",
+            left: "0vw",
+            top: `${
+              scrollPosition > 300 ? 50 - (scrollPosition - 300) * 1 : 50
+            }px`,
+          }}
+        >
+          <div className="flex w-full h-[770px] flex-col justify-end">
+            <span className="text-[42px] font-[700] text-white font-Athiti px-[18px] leading-[50.40px]">
+              “<span className="text-[#67BE4D]">ตั้งต้นใหม่</span>
+              <br />
+              เมื่อไร่ประสบภัย
+              <br />
+              ด้วยประกันต้นไม้
+              <br />
+              ไว้ใจ
+              <span className="text-[#67BE4D]">การันTree</span>”
+            </span>
+            <span className="font-Athiti text-[20px] font-[500] text-white px-[18px] mt-[12px]">
+              ครอบคลุมทุกภัยพิบัติ ไม่มีค่าใช้จ่าย สมัครง่าย
+            </span>
+          </div>
+        </div>
+        <Button
+          className="flex mt-[1120px] mb-[40px] h-[60px] w-[340px] max-w-[90%] ml-[18px] bg-[#67BE4D] hover:bg-[#457536] z-[5]"
+          onClick={toBottom}
+        >
+          ลองทำนายที่ดินดูเลย
+        </Button>
+        <div className="flex w-full h-[100vh] bg-black fixed top-[0px] z-[-30]" />
+        <Step />
+        <div className="relative flex w-full h-[100px] z-[-10] mb-[-2px]">
+          <Image src="/images/CurveBar.png" alt="Picture of the land" fill />
+        </div>
+        <div className="flex flex-col w-full h-fit bg-[#67BE4D] pb-[20px]">
+          <Helping scrollPosition={scrollPosition} />
+        </div>
+        <div className="w-full h-fit bg-white pt-[40px]">
+          <Sponsor />
+          <div className="flex flex-col w-full h-fit relative max-w-[500px] mx-auto">
+            <NewCard />
+          </div>
+        </div>
+        <div className="flex w-full h-[150px] bg-[#295729]"></div>
       </div>
-      <div className="flex w-full h-[150px] bg-[#295729]"></div>
-    </div>
+    </>
   )
 }
 
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -9,21 +9,26 @@ import {
 } from "@/components/ui/sheet"
 import Burger from "@/modules/Burger"
 import Navbar from "@/modules/Navbar"
+
 import "@/styles/global.css"
 import type { AppProps } from "next/app"
+
 import Image from "next/image"
+import Link from "next/link"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className="min-h-screen">
       <div className="flex w-full h-[64px] items-center justify-center px-[24px] fixed top-0 z-[10] bg-white">
         <div className="mr-auto">
-          <Image
-            src="/images/LOGO.png"
-            alt="Picture of the land"
-            width={32}
-            height={32}
-          />
+          <Link href="/">
+            <Image
+              src="/images/LOGO.png"
+              alt="Picture of the land"
+              width={32}
+              height={32}
+            />
+          </Link>
         </div>
         <Button className="bg-green-700 hover:bg-green-700 mr-[17px]">
           ติดต่อเรา
